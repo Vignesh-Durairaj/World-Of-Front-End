62fd59060e86934ab1a71604557a871903bf291f
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-01-17 12:48:56 +0530

    Added few other jQuery exercises

--- WebContent/js/jquery-test.js
+++ WebContent/js/jquery-test.js
@@ -1,4 +1,4 @@
-function say_hello(){
+function say_hello() {
 	textVal = $('#myText').val()
 	if (textVal) {
 		alert('Hello ' + textVal)
@@ -7,6 +7,19 @@ function say_hello(){
 	}
 }
 
-function change_content(){
-	$('#myDiv1').html('New Value');
+function change_content() {
+	$('#myDiv1').html('<b>New Value</b>');
+}
+
+function change_attr() {
+	$('.dark').attr('class','light');
+	$('a').removeAttr('href');
+	$('a.newLink').attr('href', 'https://www.sc.com/in/');
+}
+
+function ammend() {
+	$('#myDiv2').append(' Another Value appended');
+	$('#myDiv2').prepend('Hi! ');
+	$('#myDiv2').before('<hr style="border-top: 2px solid orange;"/>');
+	$('#myDiv2').after('<hr style="border-top: 2px solid green;"/>');
 }
