1215320d8e2aaf2f9e66bad84bafff721bed398e
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-04-30 13:18:03 +0530

    Added difference between template literals and strings

--- WebContent/js/es6/template_strings.js
+++ WebContent/js/es6/template_strings.js
@@ -2,14 +2,14 @@ window.onload = function() {
 	
 	let normal_string = "String one is here. Let's see how it works";
 
-	let another_string = 'Another string is here. \n And the new line is here'
+	let another_string = 'Another string is here. \n And the new line is here';
 
 	console.log(normal_string);
 	console.log(another_string);
 
 	function templateStrings() {
 		let template_string = `This is a template string!
-		And it allows a new line. It's fun`;
+		And it allows a new line. It's fun`; // New line will not be escaped
 
 		console.log(template_string);
 	}
@@ -19,6 +19,12 @@ window.onload = function() {
 	function printPerson(name, age) {
 		// Here we use ${} operator to get a handle on the variables.
 		console.log(`The name is ${name} and the age is ${age}`);
+
+		// And guess what ... we can even evaluate some mathematical expression within variables
+		console.log(`The sum value is ${1 + 4}`);
+
+		// Where as normal way is ..
+		console.log('The sum value is ' + (1 + 4)); // More annoying isn't it ?
 	}
 
 	printPerson('Vignesh', 32);
