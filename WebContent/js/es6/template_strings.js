167656ec57180316ddc11605928891cb136107d5
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-04-30 13:20:58 +0530

    Escaping charatecters in template strings

--- WebContent/js/es6/template_strings.js
+++ WebContent/js/es6/template_strings.js
@@ -12,6 +12,15 @@ window.onload = function() {
 		And it allows a new line. It's fun`; // New line will not be escaped
 
 		console.log(template_string);
+
+		// And guess what ... we can even evaluate some mathematical expression within variables
+		console.log(`The sum value is ${1 + 4}`);
+
+		// Where as normal way is ..
+		console.log('The sum value is ' + (1 + 4)); // More annoying isn't it ?
+
+		// How to print the special charactes and escaping literals
+		console.log(`The variables are enclosed inside \${} operator. And slashes will be '\\'`);
 	}
 
 	templateStrings();
@@ -19,12 +28,6 @@ window.onload = function() {
 	function printPerson(name, age) {
 		// Here we use ${} operator to get a handle on the variables.
 		console.log(`The name is ${name} and the age is ${age}`);
-
-		// And guess what ... we can even evaluate some mathematical expression within variables
-		console.log(`The sum value is ${1 + 4}`);
-
-		// Where as normal way is ..
-		console.log('The sum value is ' + (1 + 4)); // More annoying isn't it ?
 	}
 
 	printPerson('Vignesh', 32);
