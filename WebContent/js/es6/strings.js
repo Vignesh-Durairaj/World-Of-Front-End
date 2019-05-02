896c7fa5a50b9eccc16957455f350151ce1d92e8
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-05-02 09:33:12 +0530

    Added template for String methods

---
+++ WebContent/js/es6/strings.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+	console.log("Let's see some new elements in ECMA6 Scripts for Strings !");
+
+	document.getElementById('process').onclick = function() {
+		let textStr = document.getElementById('sampleText').value;
+
+		console.log(textStr);
+	}
+}
