418c20e1c35a91add6859c085c4dbdbc91dfe79f
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-05-03 18:41:21 +0530

    Basic exercises for ECMA6 Sets

--- WebContent/js/es6/set.js
+++ WebContent/js/es6/set.js
@@ -1,3 +1,21 @@
 window.onload = function() {
 	console.log(`Time to learn SETs in javascripts`);
+
+	let set_one = new Set("Vignesh Durairaj");
+	console.log(set_one);
+
+	console.log(set_one.has(" "));
+	if (set_one.has(' ')) {
+		set_one.delete(' ');
+	}
+
+	console.log(set_one);
+	set_one.clear();
+	console.log(set_one);
+
+
+	// Let add some more literals
+	set_one.add("Vignesh").add("Pranov").add("Venu").add("Durairaj").add("Pranov");
+	console.log(set_one);
+	console.log(set_one.size);
 }
