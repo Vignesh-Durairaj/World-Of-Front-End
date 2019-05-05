9c4a6e071c0708b4a8119eef60a24a6230fa1658
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-05-05 18:51:02 +0530

    Mid level exercises for ECMA6 Sets

--- WebContent/js/es6/set.js
+++ WebContent/js/es6/set.js
@@ -1,6 +1,12 @@
 window.onload = function() {
 	console.log(`Time to learn SETs in javascripts`);
 
+	let getDetails = function(a) {
+		console.log(a);
+		console.log(a.size);
+		console.log(a.length);
+	}
+
 	let set_one = new Set("Vignesh Durairaj");
 	console.log(set_one);
 
@@ -13,10 +19,14 @@ window.onload = function() {
 	set_one.clear();
 	console.log(set_one);
 
-
 	// Let add some more literals
 	set_one.add("Vignesh").add("Pranov").add("Venu").add("Durairaj").add("Pranov");
-	console.log(set_one);
-	console.log(set_one.size); 
-	
+	getDetails(set_one);
+
+	// A set can be created either as empty or for any iterable elements
+	// Let's filter an array with unique elements
+
+	let my_arr = ['Vignesh', 'Durairaj', 'Pranov', 'Venu', 'Pradeep', 'Pranov', 'Teju', 'Sachin', 'Venu'];
+	getDetails(my_arr);
+	getDetails([...(new Set(my_arr))]);
 }
