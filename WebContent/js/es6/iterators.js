6a4f58179b97c5a9f6cb68ec2684d4b632b0820f
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-05-06 19:16:13 +0530

    =Added exercises for iterators in JS

--- WebContent/js/es6/iterators.js
+++ WebContent/js/es6/iterators.js
@@ -1,3 +1,49 @@
 window.onload = function() {
 	console.log(`Now... we get in detail with iterators`);
+
+	const devs = [
+		'brown_wizard', 
+		'mad_coder',
+		'igor_vanyberg',
+		'linus_torvalds'
+	];
+
+	// The usual way is iterating using loops
+	// Like FOR, WHILE and FOR-OF
+	for (dev of devs) {
+		console.log(`The developer name : ${dev}`);
+	}
+
+	// Let's make this DEVS array as another type of iterable
+	const devIter = {
+		[Symbol.iterator]() {
+			let step = 0;
+			const iterator = {
+				next() {
+					step ++;
+
+					if (step === 1) {
+						return devs[step - 1];
+					} else if (step === 2) {
+						return devs[step - 1];
+					} else if (step === 3) {
+						return devs[step - 1];
+					} else if (step === 4) {
+						return devs[step - 1];
+					}
+
+					return null;
+				}
+			};
+
+			return iterator;
+		}
+	};
+
+	let myIterator = devIter[Symbol.iterator]();
+	console.log('From new Iterator : ' + myIterator.next());
+	console.log('From new Iterator : ' + myIterator.next());
+	console.log('From new Iterator : ' + myIterator.next());
+	console.log('From new Iterator : ' + myIterator.next());
+
 }
